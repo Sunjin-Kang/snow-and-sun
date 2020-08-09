f46b7d3d8c0d7cc92f238e0d0ca4c41cd1bdecf8
Sunjin Kang <sunjin@bu.edu>
2020-08-09 10:42:56 -0400

    first commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = props => {
+    return (
+        <main>
+            <div>
+                
+            </div>
+        </main>
+    )
+}
+
+export default Landing
